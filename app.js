b6e323cf96a2943a0d580a0d83646f4344fed376
Raghav Gupta <rg1997@srmist.edu.in>
2025-03-20 14:35:36 +0530

    node

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
